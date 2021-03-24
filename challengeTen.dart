d0da7c054e3368bf5a0e1d6e59ece8db750523f8
David Kim <34044661+Dav1212@users.noreply.github.com>
2021-03-24 14:55:11 -0400

    Create challengeTen.dart

---
+++ challengeTen.dart
@@ -0,0 +1,39 @@
+class MyApp extends StatelessWidget {
+  
+
+  static const String _title = 'Card';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body:  MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: const SizedBox(
+            width: 300,
+            height: 100,
+            child: Text('A card that can be tapped'),
+          ),
+        ),
+      ),
+    );
+  }
+}
